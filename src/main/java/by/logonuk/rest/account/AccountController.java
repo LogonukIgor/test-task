79cb16504568ab44a6f310921a4b0e9c82f98768
Igor Logonuk <logonuk16@mail.ru>
2024-02-02 11:41:13 +0300

    Initial commit

---
+++ src/main/java/by/logonuk/rest/account/AccountController.java
@@ -0,0 +1,4 @@
+package by.logonuk.rest.account;
+
+public interface AccountController {
+}
---
+++ src/main/java/by/logonuk/rest/account/AccountControllerImpl.java
@@ -0,0 +1,9 @@
+package by.logonuk.rest.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountControllerImpl implements AccountController{
+}
